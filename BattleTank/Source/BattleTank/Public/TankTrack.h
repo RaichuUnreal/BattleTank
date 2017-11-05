d511a0062e6a2f56a671c0ffe3ad6a52f36c965a
RaichuBender <t-bender@hotmail.com>
2017-11-05 18:07:44 +0100

    Added TankTrack components, added left and right throttle axis bindings, added SetThrottle method

---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,21 @@
+// Copyright 2017 Thomas Gijsbers
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+// Inherited from UStaticMeshComponent.
+// TankTrack is used to set maximum driving force, and to apply forces to the tank.
+UCLASS( meta = (BlueprintSpawnableComponent) )
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	// Sets a throttle between -1 and +1
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+};
