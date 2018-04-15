0b244e4ffd6cab0b97a0384107324a107ef6e2b2
Thomas Gijsbers <t-bender@hotmail.com>
2018-04-15 04:15:16 +0200

    Added TrackThrottle input binding for keyboard arrows, made throttle work, added track's collision mesh, tweaked tank base's collision mesh, added physics material to change friction and type, changed Max Driving Force

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -8,7 +8,11 @@ void UTankTrack::SetThrottle(float Throttle)
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
 
-	// Todo clamp actual throttle value so player can't over-drive
+	// TODO clamp actual throttle value so player can't over-drive
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied,ForceLocation);
 }
 
 
