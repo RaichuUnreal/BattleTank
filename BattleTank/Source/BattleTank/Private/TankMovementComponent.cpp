4d2f741e86c092d69a5188f66fd93a6d132fd9eb
Thomas Gijsbers <t-bender@hotmail.com>
2018-04-15 20:17:02 +0200

    Made TankMovement BlueprintCallable, replaced default subobject with callable, added Left- and RightTrack to the TankMovementComponent, added functionality to IntendMoveForward

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,11 +1,23 @@
 // Copyright 2018 Thomas Gijsbers
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	/// auto Time = GetWorld()->GetTimeSeconds();
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+
+	// TODO prevent doubble speed due to dual control use
 }
 
 
