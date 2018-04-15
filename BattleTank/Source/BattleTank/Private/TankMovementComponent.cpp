614fa1f124bbff3e40208567834440ddb3b085e8
Thomas Gijsbers <t-bender@hotmail.com>
2018-04-15 17:38:45 +0200

    Changed Copyright notice, added MoveForward inpit binding, added TankMovementComponent with IntendMoveForward function, added IntendMoveForward to input MoveForward in the Tank's blueprint

---
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -0,0 +1,11 @@
+// Copyright 2018 Thomas Gijsbers
+
+#include "TankMovementComponent.h"
+
+void UTankMovementComponent::IntendMoveForward(float Throw)
+{
+	/// auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+}
+
+
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -11,6 +11,7 @@
 class UTankBarrel;
 class UTankTurret;
 class UTankAimingComponent;
+class UTankMovementComponent;
 class AProjectile;
 
 UCLASS()
@@ -35,6 +36,9 @@ protected:
 	virtual void BeginPlay() override;
 
 	UTankAimingComponent* TankAimingComponent = nullptr;
+
+	UPROPERTY(BlueprintReadOnly)
+	UTankMovementComponent* TankMovementComponent = nullptr;
 
 private:	
 	// Sets default values for this pawn's properties
